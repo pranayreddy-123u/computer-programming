c71f94c88c62a082121298bb7409808276cf9030
pranayreddy-123u <maramreddy.kumar_cs.h24@gla.ac.in>
2024-11-28 14:01:20 +0530

    Create dict_q4.py

---
+++ coding-assignment/dict/dict_q4.py
@@ -0,0 +1,5 @@
+#How do you remove a key-value pair from a dictionary?
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+removed_value = my_dict.pop('key1')
+print(my_dict)         
+print(removed_value)  
