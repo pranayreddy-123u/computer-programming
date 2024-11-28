bbd486deacc6d9e95755343586425bbdc9847d6d
pranayreddy-123u <maramreddy.kumar_cs.h24@gla.ac.in>
2024-11-28 13:59:13 +0530

    Create dict_q3.py

---
+++ coding-assignment/dict/dict_q3.py
@@ -0,0 +1,8 @@
+# How do you check if a key exists in a dictionary?
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+key_to_check = 'key1'
+
+if key_to_check in my_dict:
+    print(f"{key_to_check} exists!")  
+else:
+    print(f"{key_to_check} does not exist.")
