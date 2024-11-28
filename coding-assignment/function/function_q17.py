9c3e8bcf049b95796e69b69e18e7f7bd6165a64d
pranayreddy-123u <maramreddy.kumar_cs.h24@gla.ac.in>
2024-11-28 22:31:40 +0530

    Create function_q17.py

---
+++ coding-assignment/function/function_q17.py
@@ -0,0 +1,12 @@
+# Create a function to serialize and deserialize a binary tree
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def serialize(root):
+    pass
+
+def deserialize(data):
+    pass
